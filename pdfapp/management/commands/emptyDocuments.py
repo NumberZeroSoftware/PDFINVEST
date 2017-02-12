ad27a00aaf45c21c5585e62a1af2c97692fe3f40
Christian Oliveros <01christianol01@gmail.com>
2017-02-12 19:53:15 -0400

    Help for command emptyDocuments

--- pdfapp/management/commands/emptyDocuments.py
+++ pdfapp/management/commands/emptyDocuments.py
@@ -1,10 +1,15 @@
+"""
+Command to delete everything
+Made by Christian Oliveros ( oliveroschristian.wordpress.com)
+Last edit 02/12/2017
+"""
 from django.core.management.base import BaseCommand
 from pdfapp.models import Document
 from django.core.files.storage import default_storage
 import sys
 
 class Command(BaseCommand):
-
+    help="Delete all the documents. Use with care"
     def handle(self, *args, **options):
         print("WARNING DELETING ALL THE DOCUMENTS", file=sys.stderr)
         print("Sure? (y/n): ", end="", file=sys.stderr)
@@ -19,5 +24,7 @@ class Command(BaseCommand):
             print("Done")
         else:
             print("Nothing Deleted", file=sys.stderr)
+
+
 
         
