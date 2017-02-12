f3ae45515c25a08378bc2cbf47dacc2bd8ded548
Christian Oliveros <01christianol01@gmail.com>
2017-02-12 18:18:03 -0400

    Resize

--- pdfapp/static/hide.js
+++ pdfapp/static/hide.js
@@ -9,13 +9,31 @@
 function showStuff(id, btn) {
     var displayStyle = document.getElementById(id).style.display;
     if (displayStyle != 'none') {
-        setCookie(id+"cssStyleDisplay", displayStyle, 1);
+        setCookie(id+"_cssStyleDisplay", displayStyle, 1);
         document.getElementById(id).style.display = 'none';
         btn.value = "Show " + btn.name;
     }
     else {
-        document.getElementById(id).style.display = getCookie(id+"cssStyleDisplay");
+        document.getElementById(id).style.display = getCookie(id+"_cssStyleDisplay");
         btn.value = "Hide " + btn.name;
     }
-    return false;
+}
+
+function resize(idList) {
+    var visibles = [];
+    for (var i = idList.length - 1; i >= 0; i--) {
+        var element = document.getElementById(idList[i]);
+        if (element.style.display != 'none') {
+            visibles.push(element);
+        }
+    }
+
+    var percentage = 100.0;
+    if (visibles.length != 0) {
+        percentage =  percentage / visibles.length;
+    }
+    
+    for (var i = visibles.length - 1; i >= 0; i--) {
+        visibles[i].style.width = percentage.toString()+"%";
+    }
 }
