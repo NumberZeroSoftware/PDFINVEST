db870bb86e98000e3aa29b664237b9fe0e0f854c
Christian Oliveros <01christianol01@gmail.com>
2017-02-18 13:57:39 -0400

    Better Server Start and Reload System

--- pdfapp/models.py
+++ pdfapp/models.py
@@ -5,5 +5,5 @@ from .validators import validate_pdf_extension
 class Document(models.Model):
     docfile = models.FileField(upload_to='documents/%Y/%m/%d',
                                validators=[validate_pdf_extension])
-    ready_html = False
-    processing_html = False
+    ready_html = models.BooleanField(default=False)
+    processing_html = models.BooleanField(default=False)
