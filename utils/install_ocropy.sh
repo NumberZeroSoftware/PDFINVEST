4fe8c03070af3e27fedc5cf0adb607e5e03a2a66
Christian Oliveros <01christianol01@gmail.com>
2017-03-03 00:59:08 -0400

    The little boy is learning

--- utils/install_ocropy.sh
+++ utils/install_ocropy.sh
@@ -1,6 +1,13 @@
+mkdir ocropy
+cd ocropy
+git clone https://github.com/tmbdev/ocropy.git
 sudo apt-get install $(cat PACKAGES)
 wget -nd http://www.tmbdev.net/en-default.pyrnn.gz
 mv en-default.pyrnn.gz models/
 sudo python setup.py install
+cd ..
+sudo cp Ocropy_Modificado/chars.py  /usr/local/lib/python2.7/dist-packages/ocrolib
+cp Ocropy_Modificado/PDFINVEST6.pyrnn.gz ocropy/models
+sudo cp Ocropy_Modificado/PDFINVEST6.pyrnn.gz /usr/local/share/ocropus/
 # Need the git clone of the ocropy repo
 # Must be inside ocropy folder
