8b82e89a3934d1fe083ecb59bfa77f21a8f7eaf0
Christian Oliveros <01christianol01@gmail.com>
2017-03-03 01:14:50 -0400

    We can now update only the installed ocropy

--- utils/install_ocropy.sh
+++ utils/install_ocropy.sh
@@ -6,8 +6,8 @@ wget -nd http://www.tmbdev.net/en-default.pyrnn.gz
 mv en-default.pyrnn.gz models/
 sudo python setup.py install
 cd ..
+./update_ocropy.sh
 sudo cp Ocropy_Modificado/chars.py  /usr/local/lib/python2.7/dist-packages/ocrolib
-cp Ocropy_Modificado/PDFINVEST6.pyrnn.gz ocropy/models
 sudo cp Ocropy_Modificado/PDFINVEST6.pyrnn.gz /usr/local/share/ocropus/
 # Need the git clone of the ocropy repo
 # Must be inside ocropy folder
