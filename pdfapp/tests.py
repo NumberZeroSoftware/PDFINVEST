56525d26a94425a9399642f1736928f03a6e1629
cinfante <cinfante@localhost.localdomain>
2017-03-16 14:15:03 -0400

    Final unit tests.

--- pdfapp/tests.py
+++ pdfapp/tests.py
@@ -13,7 +13,7 @@ class TestProgram(TestCase):
 
     # Prueba de esquina: Código numérico muy largo.
     def test_number_long(self):
-        with self.assertRaises(Exception):
+        with self.assertRaises(ValidationError):
             Program.objects.create(number="12345")
 
     # Prueba de borde: Tamaño máximo del código numérico.
@@ -22,7 +22,7 @@ class TestProgram(TestCase):
 
     # Prueba de esquina: Código numérico muy corto.
     def test_number_short(self):
-        with self.assertRaises(Exception):
+        with self.assertRaises(ValidationError):
             Program.objects.create(number="12")
 
     # Prueba de borde: Tamaño mínimo del código numérico.
@@ -31,7 +31,7 @@ class TestProgram(TestCase):
 
     # Prueba de esquina: Créditos negativos.
     def test_credits_neg(self):
-        with self.assertRaises(Exception):
+        with self.assertRaises(ValidationError):
             Program.objects.create(credits=-1)
 
     # Prueba de borde: Créditos mínimos.
@@ -44,20 +44,20 @@ class TestProgram(TestCase):
 
     # Prueba de esquina: Demasiados créditos.
     def test_credits_big(self):
-        with self.assertRaises(Exception):
+        with self.assertRaises(ValidationError):
             Program.objects.create(credits=17)
 
     # Prueba de esquina: Horas negativas.
     def test_theory_hours_neg(self):
-        with self.assertRaises(Exception):
+        with self.assertRaises(ValidationError):
             Program.objects.create(theory_hours=-1)
 
     def test_practice_hours_neg(self):
-        with self.assertRaises(Exception):
+        with self.assertRaises(ValidationError):
             Program.objects.create(practice_hours=-1)
 
     def test_laboratory_hours_neg(self):
-        with self.assertRaises(Exception):
+        with self.assertRaises(ValidationError):
             Program.objects.create(laboratory_hours=-1)
 
     # Prueba de borde: Horas en cero.
@@ -74,7 +74,7 @@ class TestProgram(TestCase):
 
     # Prueba de esquina: Demasiadas horas totales.
     def test_excess_hours(self):
-        with self.assertRaises(Exception):
+        with self.assertRaises(ValidationError):
             Program.objects.create(theory_hours=20,
                                     practice_hours=16,
                                     laboratory_hours=5)
@@ -95,24 +95,24 @@ class TestProgram(TestCase):
 
     # Prueba de esquina: Excedido el número de horas individualmente.
     def test_excess_theory(self):
-        with self.assertRaises(Exception):
+        with self.assertRaises(ValidationError):
             Program.objects.create(theory_hours=41,
                                     practice_hours=0,
                                     laboratory_hours=0)
     def test_excess_practice(self):
-        with self.assertRaises(Exception):
+        with self.assertRaises(ValidationError):
             Program.objects.create(theory_hours=0,
                                     practice_hours=41,
                                     laboratory_hours=0)
     def test_excess_laboratory(self):
-        with self.assertRaises(Exception):
+        with self.assertRaises(ValidationError):
             Program.objects.create(theory_hours=0,
                                     practice_hours=0,
                                     laboratory_hours=41)
 
     # Prueba de esquina: Año pequeño.
     def test_small_year(self):
-        with self.assertRaises(Exception):
+        with self.assertRaises(ValidationError):
             Program.objects.create(validity_year=1968)
 
     # Prueba de borde: Año mínimo.
@@ -127,4 +127,10 @@ class TestProgram(TestCase):
     # Prueba de borde: Fecha de validez mínima.
     def test_validation_date(self):
         Program.objects.create(validity_date_m=1, validity_date_y=1969)
+
+    # Prueba de esquina: Fecha de validez inválida.
+    def test_validation_date(self):
+        with self.assertRaises(ValidationError):
+            Program.objects.create(validity_date_d=29, validity_date_m=2, validity_date_y=1969)
+
 
